81d8bf9d26b3e37c7043b38f99797e3c5ff5029a
Krislv4nk <kristinalev4enko89@gmail.com>
2024-09-05 17:03:01 +0200

    start settings

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Background } from './../Background/Background'
-
+import { Background } from './../Background/Background';
+import { Container } from './../Container/Container';
 
 const SharedLayout = () => {
   return (
-    <>
+    <Container>
       <Background/>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </>
+    </Container>
   );
 };
 
