a1d53e31269767db573b33129927c8ccc86f0c3d
Krislv4nk <kristinalev4enko89@gmail.com>
2024-09-05 20:04:45 +0200

    complited

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import CustomClock from './../../components/MyDigitalClock/MyDigitalClock';
 import css from './FirstPage.module.css';
+import drewnex from './../../helpers/drewnex.jpg';
 
 const FirstPage = () => {
 
@@ -10,32 +11,44 @@ const FirstPage = () => {
     const requestWakeLock = async () => {
       try {
         wakeLock = await navigator.wakeLock.request('screen');
-
         wakeLock.addEventListener('release', () => {
           console.log('Wake lock was released');
         });
-
         console.log('Wake lock is active');
       } catch (err) {
         console.error(`${err.name}, ${err.message}`);
       }
     };
 
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        requestWakeLock(); 
+      } else if (wakeLock) {
+        wakeLock.release(); 
+        console.log('Wake lock released because page is not visible');
+      }
+    };
+
     if ('wakeLock' in navigator) {
-      requestWakeLock();
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+      
+      if (document.visibilityState === 'visible') {
+        requestWakeLock();
+      }
     }
 
     return () => {
       if (wakeLock) {
         wakeLock.release();
-        console.log('Wake lock released on cleanup');
       }
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, []);
 
   return (
     <div className={css.wrapper}>
-      <h2>First Page</h2>
+      <h2 className={css.header}>Nigdy martw się dzisiejszymi problemami, jutro będą nowe</h2>
+      {/* <img className={css.drewnex} src={drewnex} alt="myPhoto" /> */}
       <CustomClock />
       <p className={css.created}>Created by Krislv4nk</p>
     </div>
